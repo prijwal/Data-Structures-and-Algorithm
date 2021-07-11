b65167a0f16e097badddf598843e41546c0ca477
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-07-12 01:15:50 +0530

    Update Lower_bound_Implementation.cpp

--- Binary search/Smaller or equal elements/Lower_bound_Implementation.cpp
+++ Binary search/Smaller or equal elements/Lower_bound_Implementation.cpp
@@ -1,13 +1,17 @@
 int Solution::solve(vector<int> &a, int k) {
     int n=a.size();
     int l=0,h=n-1,m;
+    int ans=0;
     while(h>=l)
     {
         m=l+(h-l)/2;
         if(a[m]>k)
             h=m-1;
         else
+        {
             l=m+1;
+            ans=m+1;
+        }
     }
-    return l;
+    return ans;
 }
