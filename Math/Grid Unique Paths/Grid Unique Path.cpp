4581002c3515aae23cc98613e8ed96356c55403a
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-07-11 12:15:58 +0530

    Create Grid Unique Path.cpp

---
+++ Math/Grid Unique Paths/Grid Unique Path.cpp
@@ -0,0 +1,16 @@
+int Solution::uniquePaths(int n, int m) {
+    int dp[n][m];
+    memset(dp,0,sizeof(dp));
+    for(int i=0;i<n;i++)
+        dp[i][0]=1;
+    for(int i=0;i<m;i++)
+        dp[0][i]=1;
+    for(int i=1;i<n;i++)
+    {
+        for(int j=1;j<m;j++)
+        {
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        }
+    }
+    return dp[n-1][m-1];
+}
