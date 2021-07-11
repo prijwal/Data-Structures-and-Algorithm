faa997e0d233bdafdc0ed0711f36f2112736c6b0
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-07-11 23:11:34 +0530

    Create WoodCutting Mode Easy.cpp

---
+++ Binary search/WoodCutting Made Easy!/WoodCutting Mode Easy.cpp
@@ -0,0 +1,21 @@
+bool check(vector<int> &a,int k,int m)
+{
+    long val=0;
+    for(auto it:a)
+        val+=max(0,(it-m));
+    return val>=k;
+}
+int Solution::solve(vector<int> &a, int k){
+    int n=a.size();
+    int h=*max_element(a.begin(),a.end());
+    int l=0;
+    while(h>=l)
+    {
+        int m=l+(h-l)/2;
+        if(check(a,k,m))
+            l=m+1;
+        else
+            h=m-1;
+    }
+    return l-1;
+}
