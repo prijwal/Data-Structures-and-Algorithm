b6a56f54df49eaf655131d50489214afe02949d3
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-08-02 01:21:57 +0530

    Create Top View of Binary Tree.cpp

---
+++ Binary Trees/Top View of Binary Tree/Top View of Binary Tree.cpp
@@ -0,0 +1,34 @@
+map<Node*,int> mp;
+void  traverse(Node *root,int hd=0) // having distance of each node
+{
+    if(root==NULL)
+        return;
+    mp[root]=hd;
+    traverse(root->left,hd-1);
+    traverse(root->right,hd+1);
+}
+void topView(struct Node *root)
+{
+    traverse(root);
+    queue<Node*> q;
+    q.push(root);
+    map<int,int> hd;
+    while(!q.empty())  // Level order traversing
+    {
+        int n=q.size();
+        for(int i=0;i<n;i++)  // node in one level
+        {
+            Node *curr=q.front();
+            q.pop();
+            if(!hd[mp[curr]])
+                hd[mp[curr]]=curr->data;
+            if(curr->left!=NULL)
+                q.push(curr->left);
+            if(curr->right!=NULL)
+                q.push(curr->right);
+        }
+    }
+    hd[0]=root->data;
+    for(auto it:hd)
+        cout<<it.second<<" ";
+}
