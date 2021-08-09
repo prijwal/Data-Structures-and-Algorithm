359fa42a76dd32b48b890d26f3f5fb279321c49e
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-08-09 13:34:57 +0530

    Create nth Catalan Number.cpp

---
+++ Dynamic Proramming/nth Catalan Number/nth Catalan Number.cpp
@@ -0,0 +1,10 @@
+int numTrees(int n) {
+  vector<int> catalan(n+1,0);
+  catalan[0]=catalan[1]=1;
+  for(int i=2;i<=n;i++)
+  {
+    for(int j=0;j<i;j++)
+      catalan[i]+=catalan[j]*catalan[i-j-1];
+  }
+  return catalan[n];
+}
