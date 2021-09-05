f18afdeb6ca43c54c944dc715d1195e8f46a91a6
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-09-05 17:04:24 +0530

    Create Longest Repeating Subsequence.cpp

---
+++ String/Longest Repeating Subsequence/Longest Repeating Subsequence.cpp
@@ -0,0 +1,17 @@
+int LongestRepeatingSubsequence(string s1){
+		    int n=s1.length();
+		    int dp[n+1][n+1],i,j;
+		    string s2=s1;
+		    memset(dp,0,sizeof(dp));
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(s1[i-1]==s2[j-1] and i!=j)
+                    dp[i][j]=dp[i-1][j-1]+1;
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][n];
+		}
