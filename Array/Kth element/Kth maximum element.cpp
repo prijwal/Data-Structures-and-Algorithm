de22107ccd343f4ad18cb2e196e84dc4a2c3246d
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-09-21 23:36:12 +0530

    Create Kth maximum element.cpp

---
+++ Array/Kth element/Kth maximum element.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    priority_queue<int,vector<int>, greater<int> > max_heap;
+    int n,k;
+    cin>>n>>k;
+    
+    vector<int> a(n) ;
+    for(int i=0;i<n;i++)
+        cin>>a[i] ;
+    
+    for(int i=0 ;i<n ;i++)
+    {
+        if(max_heap.size()<k )
+        {
+            max_heap.push(a[i]);
+        }
+        else
+        {
+            if(max_heap.top()<a[i])
+            {
+                max_heap.pop();
+                max_heap.push(a[i]);
+            }
+        }
+    }
+    
+    cout<<max_heap.top()<<" ";
+    
+    return 0;
+}
