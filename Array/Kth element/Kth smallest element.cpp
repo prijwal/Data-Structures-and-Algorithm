fdb4b6da3477ef1c3bea6b6c3d02e3922ba2592c
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-09-21 23:32:25 +0530

    Create Kth smallest element.cpp

---
+++ Array/Kth element/Kth smallest element.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    priority_queue<int> min_heap;
+    int n,k;
+    cin>>n>>k;
+    
+    vector<int> a(n) ;
+    for(int i=0;i<n;i++)
+        cin>>a[i] ;
+    
+    for(int i=0 ;i<n ;i++)
+    {
+        if(min_heap.size()<k )
+        {
+            min_heap.push(a[i]);
+        }
+        else
+        {
+            if(min_heap.top()>a[i])
+            {
+                min_heap.pop();
+                min_heap.push(a[i]);
+            }
+        }
+    }
+    
+    cout<<min_heap.top()<<" ";
+    
+    return 0;
+}
