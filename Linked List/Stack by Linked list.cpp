fd39a74da2b2ef68a0b3c6c23f093fe59afa8040
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-09-22 00:48:07 +0530

    Create Stack by Linked list.cpp

---
+++ Linked List/Stack by Linked list.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+struct Node {
+   int data;
+   struct Node *next;
+   Node(){
+       next=NULL;
+   };
+   Node(int data)
+   {
+       this->data=data;
+       next=NULL;
+   }
+};
+Node* head = NULL;
+
+void push(int val) {
+   Node* newnode = new Node(val);
+   newnode->next = head;
+   head = newnode;
+}
+
+void pop() {
+    if(head==NULL)
+        cout<<"Stack Underflow\n";
+    else {
+      cout<<"The popped element is "<< head->data <<endl;
+      Node *temp=head;
+      head = head->next;
+      delete temp;
+    }
+}
+
+void top()
+{
+    if(head==NULL)
+        cout<<"Stack Underflow\n";
+    else
+      cout<<"The top element is "<< head->data <<endl;
+}
+
+void display() {
+   struct Node* ptr;
+   if(top==NULL)
+   cout<<"stack is empty";
+   else {
+      ptr = head;
+      cout<<"Stack elements are: ";
+      while (ptr != NULL) {
+         cout<< ptr->data <<" ";
+         ptr = ptr->next;
+      }
+   }
+   cout<<endl;
+}
+int main() {
+   int ch, val;
+   do {
+      cout<<"Enter choice: "<<endl;
+      cin>>ch;
+      cout<<"choice "<<ch<<"\n";
+      switch(ch) {
+         case 1: {
+            cout<<"Enter value to be pushed:"<<endl;
+            cin>>val;
+            push(val);
+            break;
+         }
+         case 2: {
+            pop();
+            break;
+         }
+         case 3: {
+            display();
+            break;
+         }
+         case 4: {
+             top();
+             break;
+         }
+         case 5: {
+            cout<<"Exit"<<endl;
+            break;
+         }
+         default: {
+            cout<<"Invalid Choice"<<endl;
+         }
+      }
+   }while(ch!=4);
+   return 0;
+}
