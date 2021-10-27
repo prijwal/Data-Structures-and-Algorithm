f8e97fa15c2979519ef5a25e6fae9f18c8b4dc58
Prijwal singh <63565510+prijwal@users.noreply.github.com>
2021-10-27 07:01:36 +0530

    Create Bottom view.cpp

---
+++ Binary Trees/View of binary tree/Bottom view.cpp
@@ -0,0 +1,37 @@
+map<Node*,int> mp;
+int mn=INT_MAX,mx=INT_MIN;
+void  traverse(Node *root,int hd=0) // having distance of each node
+{
+    mn=min(hd,mn);
+    mx=max(hd,mx);
+    if(root==NULL)
+        return;
+    mp[root]=hd;
+    traverse(root->left,hd-1);
+    traverse(root->right,hd+1);
+}
+vector <int> bottomView(Node *root)
+{
+    traverse(root);
+    queue<Node*> q;
+    q.push(root);
+    map<int,int> hd;
+    vector<int> ans;
+    while(!q.empty())  // Level order traversing
+    {
+        int n=q.size();
+        for(int i=0;i<n;i++)  // node in one level
+        {
+            Node *curr=q.front();
+            q.pop();
+            hd[mp[curr]]=curr->data;
+            if(curr->left!=NULL)
+                q.push(curr->left);
+            if(curr->right!=NULL)
+                q.push(curr->right);
+        }
+    }
+    for(auto it:hd)
+        ans.push_back(it.second);
+    return ans;
+}
